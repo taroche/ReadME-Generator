e0ba830a9831dc78f63295f7fc33cad421c44062
Tristan LaRoche <laroche11235@gmail.com>
2021-02-12 06:51:24 -0500

    wrote the questions I'll need for the readme

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 import * as generateMarkdown from "../utils/generateMarkdown.js";
 const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your Title?", "Please write a description of your website.", "Please write how to install website on their local machine.", "Please write what the purpose of your website is.", "Please write the contribution guidelines.", "Please write how people can test your website.", "Please enter your Github username.", "Please enter your email address."];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
