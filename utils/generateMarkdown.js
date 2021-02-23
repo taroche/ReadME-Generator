9750630722816173fe202a1c44e1935a62f12355
Tristan LaRoche <laroche11235@gmail.com>
2021-02-23 10:02:15 -0500

    moved renderbadge to inside link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,47 +1,42 @@
 
-function renderLicenseBadge(license) {
-  return `[![License: ${license}](https://img.shields.io/badge/License-${license}-red)]`
-}
 
 function renderLicenseLink(license) {
   switch(license) {
     case "Apache License 2.0":
-    return `(https://opensource.org/licenses/Apache-2.0)`
+    return `[![License:%20Apache-2.0](https://img.shields.io/badge/License-Apache%202.0-red.svg)](https://opensource.org/licenses/Apache-2.0)`
     break;
     case "BSD 3-Clause license":
-    return `(https://opensource.org/licenses/BSD-3-Clause)`
+    return `[![License:%20BSD%203-Clause](https://img.shields.io/badge/License-BSD%203-Clause-red.svg)](https://opensource.org/licenses/BSD-3-Clause)`
     break;
     case "BSD 2-Clause license":
-    return `(https://opensource.org/licenses/BSD-2-Clause)`
+    return `[![License:%20BSD%202-Clause](https://img.shields.io/badge/License-BSD%202-Clause-red.svg)](https://opensource.org/licenses/BSD-2-Clause)`
     break;
     case "GNU General Public License (GPL)":
-    return `(https://opensource.org/licenses/gpl-license)`
+    return `[![License:%20gpl-license](https://img.shields.io/badge/License-GPL%20license-red.svg)](https://opensource.org/licenses/gpl-license)`
     break;
     case `GNU Library or "Lesser" General Public License (LGPL)`:
-    return `(https://opensource.org/licenses/lgpl-license)`
+    return `[![License:%20lgpl-license](https://img.shields.io/badge/License-LGPL%20license-red.svg)](https://opensource.org/licenses/lgpl-license)`
     break;
     case "MIT license":
-    return `(https://opensource.org/licenses/MIT)`
+    return `[![License:%20MIT%20license](https://img.shields.io/badge/License-MIT%20license-red.svg)](https://opensource.org/licenses/MIT)`
     break;
     case "Mozilla Public License 2.0":
-    return `(https://opensource.org/licenses/MPL-2.0)`
+    return `[![License:%20MPL-2.0](https://img.shields.io/badge/License-MPL%202.0-red.svg)](https://opensource.org/licenses/MPL-2.0)`
     break;
     case "Common Development and Distribution License":
-    return `(https://opensource.org/licenses/CDDL-1.0)`
+    return `[![License:%20CDDL-1.0](https://img.shields.io/badge/License-CDDL%201.0-red.svg)](https://opensource.org/licenses/CDDL-1.0)`
     break;
     case "Eclipse Public License version 2.0":
-    return `(https://opensource.org/licenses/EPL-2.0)`
+    return `[![License:%20EPL-2.0](https://img.shields.io/badge/License-EPL%202.0-red.svg)](https://opensource.org/licenses/EPL-2.0)`
     break;
   }
 }
 
 function renderLicenseSection(license) { 
-  let renderedLicense = renderLicenseBadge(license)
-  let renderedLink = renderLicenseLink(license)
   return `
   ## License
 
-  ${renderedLicense}${renderedLink}`
+  ${renderLicenseLink(license)}`
 }
 
 function renderTableOfContent(inst, us, con, tes) {
